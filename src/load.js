1567bd0fa76b5362c747bec80071c96bd222604f
Bogdan Chadkin <trysound@yandex.ru>
2016-06-13 19:50:09 +0300

    Combine applyData and loader

--- src/load.js
+++ src/load.js
@@ -1,6 +1,31 @@
 import { dirname, resolve } from 'path';
 import readCache from 'read-cache';
-import applyData from './lib/apply-data';
+import assign from 'object-assign';
+import { parseDOM } from 'htmlparser2';
+import { selectAll, selectOne } from 'css-select';
+import serialize from 'dom-serializer';
+
+function applyData(source, params = {}, selectors = {}) {
+    const dom = parseDOM(source, { xmlMode: true });
+    const svg = dom ? selectOne('svg', dom) : null;
+
+    if (!svg) {
+        throw Error('Invalid loaded xml format');
+    }
+
+    assign(svg.attribs, params);
+
+    Object.keys(selectors).forEach(selector => {
+        const attribs = selectors[selector];
+        const elements = selectAll(selector, svg);
+
+        elements.forEach(element => {
+            assign(element.attribs, attribs);
+        });
+    });
+
+    return serialize(dom);
+}
 
 function resolveId(node, url, path) {
     const file = node.source && node.source.input && node.source.input.file;
@@ -16,10 +41,7 @@ function resolveId(node, url, path) {
 export default function load(item, opts) {
     const id = resolveId(item.node, item.url, opts.path);
     return readCache(id, 'utf-8').then(data => {
-        let code = applyData(data, {
-            root: item.params,
-            selectors: item.selectors
-        });
+        let code = applyData(data, item.params, item.selectors);
         if (opts.encode) {
             code = opts.encode(code);
         }
