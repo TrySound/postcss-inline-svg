7c56db18124dddc2f18a0507ec1f9e2bdc0c7e18
Bogdan Chadkin <trysound@yandex.ru>
2016-06-13 12:38:05 +0300

    Use mocha for tests

--- test/short.js
+++ test/short.js
@@ -1,4 +1,3 @@
-const test = require('ava');
 const assert = require('assert');
 const postcss = require('postcss');
 const plugin = require('../');
@@ -16,84 +15,86 @@ function compare(fixture, expected, warnings = []) {
     });
 }
 
-test('should compile basic', () => {
-    return compare(
-        `background: svg-load('fixtures/basic.svg');`,
-        `background: url("data:image/svg+xml;charset=utf-8,<svg id='basic'/>");`
-    );
-});
+describe('short syntax', () => {
+    it('should compile basic', () => {
+        return compare(
+            `background: svg-load('fixtures/basic.svg');`,
+            `background: url("data:image/svg+xml;charset=utf-8,<svg id='basic'/>");`
+        );
+    });
 
-test('should skip unexpected function syntax', () => {
-    const fixtures = `
-        background: svg-load();
-        background: svg-load('fixtures/basic.svg', );
-    `;
-    return compare(
-        fixtures,
-        fixtures,
-        [
-            'Invalid "svg-load()" definition',
-            'Invalid "svg-load()" definition'
-        ]
-    );
-});
+    it('should skip unexpected function syntax', () => {
+        const fixtures = `
+            background: svg-load();
+            background: svg-load('fixtures/basic.svg', );
+        `;
+        return compare(
+            fixtures,
+            fixtures,
+            [
+                'Invalid "svg-load()" definition',
+                'Invalid "svg-load()" definition'
+            ]
+        );
+    });
 
-test('should compile fill param', () => {
-    return compare(
-        `background: svg-load('fixtures/basic.svg', fill=#fff);`,
-        `background: url("data:image/svg+xml;charset=utf-8,<svg id='basic' fill='#fff'/>");`
-    );
-});
+    it('should compile fill param', () => {
+        return compare(
+            `background: svg-load('fixtures/basic.svg', fill=#fff);`,
+            `background: url("data:image/svg+xml;charset=utf-8,<svg id='basic' fill='#fff'/>");`
+        );
+    });
 
-test('should compile unquoted path', () => {
-    return compare(
-        `
-        background: svg-load(fixtures/basic.svg);
-        background: svg-load(fixtures/basic.svg, fill=#fff);
-        `,
-        `
-        background: url("data:image/svg+xml;charset=utf-8,<svg id='basic'/>");
-        background: url("data:image/svg+xml;charset=utf-8,<svg id='basic' fill='#fff'/>");
-        `
-    );
-});
+    it('should compile unquoted path', () => {
+        return compare(
+            `
+            background: svg-load(fixtures/basic.svg);
+            background: svg-load(fixtures/basic.svg, fill=#fff);
+            `,
+            `
+            background: url("data:image/svg+xml;charset=utf-8,<svg id='basic'/>");
+            background: url("data:image/svg+xml;charset=utf-8,<svg id='basic' fill='#fff'/>");
+            `
+        );
+    });
 
-test('should compile fill and stroke param', () => {
-    return compare(
-        `background: svg-load('fixtures/basic.svg', fill=#fff, stroke=#000);`,
-        `background: url("data:image/svg+xml;charset=utf-8,<svg id='basic' fill='#fff' stroke='#000'/>");`
-    );
-});
+    it('should compile fill and stroke param', () => {
+        return compare(
+            `background: svg-load('fixtures/basic.svg', fill=#fff, stroke=#000);`,
+            `background: url("data:image/svg+xml;charset=utf-8,<svg id='basic' fill='#fff' stroke='#000'/>");`
+        );
+    });
 
-test('should compile fill param with colon syntax', () => {
-    return compare(
-        `background: svg-load('fixtures/basic.svg', fill: #fff);`,
-        `background: url("data:image/svg+xml;charset=utf-8,<svg id='basic' fill='#fff'/>");`
-    );
-});
+    it('should compile fill param with colon syntax', () => {
+        return compare(
+            `background: svg-load('fixtures/basic.svg', fill: #fff);`,
+            `background: url("data:image/svg+xml;charset=utf-8,<svg id='basic' fill='#fff'/>");`
+        );
+    });
 
-test('should skip param with unexpected separator', () => {
-    const fixtures = `
-        background: svg-load('fixtures/basic.svg', fill: #fff, stroke=#000);
-        background: svg-load('fixtures/basic.svg', fill=#fff, stroke: #000);
-        background: svg-load('fixtures/basic.svg', fill #fff);
-        background: svg-load('fixtures/basic.svg', fill-#fff);
-    `;
-    return compare(
-        fixtures,
-        fixtures,
-        [
-            'Expected ":" separator in "stroke=#000"',
-            'Expected "=" separator in "stroke: #000"',
-            'Expected ":" or "=" separator in "fill #fff"',
-            'Expected ":" or "=" separator in "fill-#fff"'
-        ]
-    );
-});
+    it('should skip param with unexpected separator', () => {
+        const fixtures = `
+            background: svg-load('fixtures/basic.svg', fill: #fff, stroke=#000);
+            background: svg-load('fixtures/basic.svg', fill=#fff, stroke: #000);
+            background: svg-load('fixtures/basic.svg', fill #fff);
+            background: svg-load('fixtures/basic.svg', fill-#fff);
+        `;
+        return compare(
+            fixtures,
+            fixtures,
+            [
+                'Expected ":" separator in "stroke=#000"',
+                'Expected "=" separator in "stroke: #000"',
+                'Expected ":" or "=" separator in "fill #fff"',
+                'Expected ":" or "=" separator in "fill-#fff"'
+            ]
+        );
+    });
 
-test('should override fill param', () => {
-    return compare(
-        `background: svg-load('fixtures/basic-black.svg', fill=#fff);`,
-        `background: url("data:image/svg+xml;charset=utf-8,<svg id='basic-black' fill='#fff'/>");`
-    );
+    it('should override fill param', () => {
+        return compare(
+            `background: svg-load('fixtures/basic-black.svg', fill=#fff);`,
+            `background: url("data:image/svg+xml;charset=utf-8,<svg id='basic-black' fill='#fff'/>");`
+        );
+    });
 });
