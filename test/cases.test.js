d1b9db52487f9d30356f4fff3020596b5e73c6dc
Bogdan Chadkin <trysound@yandex.ru>
2018-03-23 13:24:51 +0300

    Prefer jest test over describe/it

--- test/cases.test.js
+++ test/cases.test.js
@@ -4,108 +4,106 @@ const { compare } = require("./utils.js");
 
 process.chdir(__dirname);
 
-describe("cases", () => {
-  it("should resolve quotes in transform step", () => {
-    return compare(
-      `background: svg-load('fixtures/font.svg');`,
-      `background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' font='%22Nelvetica Neue%22, sans-serif'/>");`,
-      { from: "input.css", encode: false }
-    );
-  });
+test("should resolve quotes in transform step", () => {
+  return compare(
+    `background: svg-load('fixtures/font.svg');`,
+    `background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' font='%22Nelvetica Neue%22, sans-serif'/>");`,
+    { from: "input.css", encode: false }
+  );
+});
 
-  it("should stringify all values after ast transformation", () => {
-    return compare(
-      `
-      @svg-load icon url(fixtures/basic.svg) {}
-      background: svg-inline(icon);
-      background: svg-load('fixtures/basic.svg');
-      `,
-      `
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
-      `,
-      { from: "input.css", encode: false }
-    ).then(result => {
-      result.root.walkDecls(decl => {
-        expect(typeof decl.value).toEqual("string");
-      });
+test("should stringify all values after ast transformation", () => {
+  return compare(
+    `
+    @svg-load icon url(fixtures/basic.svg) {}
+    background: svg-inline(icon);
+    background: svg-load('fixtures/basic.svg');
+    `,
+    `
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
+    `,
+    { from: "input.css", encode: false }
+  ).then(result => {
+    result.root.walkDecls(decl => {
+      expect(typeof decl.value).toEqual("string");
     });
   });
+});
 
-  it("should warn on not found file", () => {
-    return compare(
-      `background: svg-load('fixtures/not-found.svg');`,
-      `background: svg-load('fixtures/not-found.svg');`,
-      { from: "input.css", encode: false },
-      [`Can't load '${path.resolve("fixtures/not-found.svg")}'`]
-    );
+test("should warn on not found file", () => {
+  return compare(
+    `background: svg-load('fixtures/not-found.svg');`,
+    `background: svg-load('fixtures/not-found.svg');`,
+    { from: "input.css", encode: false },
+    [`Can't load '${path.resolve("fixtures/not-found.svg")}'`]
+  );
+});
+
+test("should add message about dependency", () => {
+  return compare(
+    `
+    background: svg-load('fixtures/basic.svg');
+    @svg-load icon url('fixtures/basic-black.svg') {}
+    `,
+    `
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>")
+    `,
+    { from: "input.css", encode: false }
+  ).then(result => {
+    const messages = result.messages
+      .filter(message => message.type === "dependency")
+      .map(message => [message.file, message.parent])
+      .sort();
+    expect(messages).toEqual([
+      [path.resolve("fixtures/basic-black.svg"), path.resolve("input.css")],
+      [path.resolve("fixtures/basic.svg"), path.resolve("input.css")]
+    ]);
   });
+});
 
-  it("should add message about dependency", () => {
-    return compare(
-      `
-      background: svg-load('fixtures/basic.svg');
-      @svg-load icon url('fixtures/basic-black.svg') {}
-      `,
-      `
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>")
-      `,
-      { from: "input.css", encode: false }
-    ).then(result => {
-      const messages = result.messages
-        .filter(message => message.type === "dependency")
-        .map(message => [message.file, message.parent])
-        .sort();
-      expect(messages).toEqual([
-        [path.resolve("fixtures/basic-black.svg"), path.resolve("input.css")],
-        [path.resolve("fixtures/basic.svg"), path.resolve("input.css")]
-      ]);
-    });
+test("should not add message about not found dependency", () => {
+  return compare(
+    `
+    background: svg-load('fixtures/not-found.svg');
+    @svg-load icon url('fixtures/not-found.svg') {}
+    `,
+    `
+    background: svg-load('fixtures/not-found.svg');
+    @svg-load icon url('fixtures/not-found.svg') {}
+    `,
+    { from: "input.css", encode: false },
+    [
+      `Can't load '${path.resolve("fixtures/not-found.svg")}'`,
+      `Can't load '${path.resolve("fixtures/not-found.svg")}'`
+    ]
+  ).then(result => {
+    const messages = result.messages
+      .filter(message => message.type === "dependency")
+      .map(message => message.file)
+      .sort();
+    expect(messages).toEqual([]);
   });
+});
 
-  it("should not add message about not found dependency", () => {
-    return compare(
-      `
-      background: svg-load('fixtures/not-found.svg');
-      @svg-load icon url('fixtures/not-found.svg') {}
-      `,
-      `
-      background: svg-load('fixtures/not-found.svg');
-      @svg-load icon url('fixtures/not-found.svg') {}
-      `,
-      { from: "input.css", encode: false },
-      [
-        `Can't load '${path.resolve("fixtures/not-found.svg")}'`,
-        `Can't load '${path.resolve("fixtures/not-found.svg")}'`
-      ]
-    ).then(result => {
-      const messages = result.messages
-        .filter(message => message.type === "dependency")
-        .map(message => message.file)
-        .sort();
-      expect(messages).toEqual([]);
-    });
-  });
-
-  it("should add parent in dependency message if specified", () => {
-    return compare(
-      `
-      background: svg-load('basic.svg');
-      @svg-load icon url('basic-black.svg') {}
-      `,
-      `
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>")
-      `,
-      { from: "fixtures/file.css", encode: false }
-    ).then(result => {
-      const messages = result.messages
-        .filter(message => message.type === "dependency")
-        .map(message => message.parent)
-        .sort();
-      expect(messages).toEqual([
-        path.resolve("fixtures/file.css"),
-        path.resolve("fixtures/file.css")
-      ]);
-    });
+test("should add parent in dependency message if specified", () => {
+  return compare(
+    `
+    background: svg-load('basic.svg');
+    @svg-load icon url('basic-black.svg') {}
+    `,
+    `
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>")
+    `,
+    { from: "fixtures/file.css", encode: false }
+  ).then(result => {
+    const messages = result.messages
+      .filter(message => message.type === "dependency")
+      .map(message => message.parent)
+      .sort();
+    expect(messages).toEqual([
+      path.resolve("fixtures/file.css"),
+      path.resolve("fixtures/file.css")
+    ]);
   });
 });
--- test/options.test.js
+++ test/options.test.js
@@ -3,174 +3,172 @@ const { compare } = require("./utils.js");
 
 process.chdir(__dirname);
 
-describe("options", () => {
-  it('should take file relatively to "from" option', () => {
-    return compare(
-      `
-      @svg-load icon url(basic.svg) {}
-      background: svg-load('basic.svg');
-      background: svg-inline(icon);
-      `,
-      `
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
-      `,
-      { from: "./fixtures/index.css", encode: false }
-    );
-  });
+test('should take file relatively to "from" option', () => {
+  return compare(
+    `
+    @svg-load icon url(basic.svg) {}
+    background: svg-load('basic.svg');
+    background: svg-inline(icon);
+    `,
+    `
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
+    `,
+    { from: "./fixtures/index.css", encode: false }
+  );
+});
 
-  it('should take file relatively to "path" option', () => {
-    return compare(
-      `
-      @svg-load icon url(basic.svg) {}
-      background: svg-load('basic.svg');
-      background: svg-inline(icon);
-      `,
-      `
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
-      `,
-      { from: 'input.css', path: "./fixtures", encode: false }
-    );
-  });
+test('should take file relatively to "path" option', () => {
+  return compare(
+    `
+    @svg-load icon url(basic.svg) {}
+    background: svg-load('basic.svg');
+    background: svg-inline(icon);
+    `,
+    `
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
+    `,
+    { from: "input.css", path: "./fixtures", encode: false }
+  );
+});
 
-  it('should prefer "path" option over "from"', () => {
-    return compare(
-      `
-      @svg-load icon url(basic.svg) {}
-      background: svg-load('basic.svg');
-      background: svg-inline(icon);
-      `,
-      `
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
-      `,
-      { from: "./fixtures/deeper/index.css", path: "./fixtures", encode: false }
-    );
-  });
+test('should prefer "path" option over "from"', () => {
+  return compare(
+    `
+    @svg-load icon url(basic.svg) {}
+    background: svg-load('basic.svg');
+    background: svg-inline(icon);
+    `,
+    `
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/>");
+    `,
+    { from: "./fixtures/deeper/index.css", path: "./fixtures", encode: false }
+  );
+});
 
-  it("should ignore xmlns absence", () => {
-    return compare(
-      `background: svg-load('fixtures/basic.svg');`,
-      `background: url("data:image/svg+xml;charset=utf-8,%3Csvg id='basic'/%3E");`,
-      { from: 'input.css', xmlns: false }
-    );
-  });
+test("should ignore xmlns absence", () => {
+  return compare(
+    `background: svg-load('fixtures/basic.svg');`,
+    `background: url("data:image/svg+xml;charset=utf-8,%3Csvg id='basic'/%3E");`,
+    { from: "input.css", xmlns: false }
+  );
+});
 
-  it("should skip adding of xmlns if it is present in file", () => {
-    return compare(
-      `background: svg-load('fixtures/basic-with-xmlns.svg');`,
-      `background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic-with-xmls'/>");`,
-      { from: 'input.css', encode: false }
-    );
-  });
+test("should skip adding of xmlns if it is present in file", () => {
+  return compare(
+    `background: svg-load('fixtures/basic-with-xmlns.svg');`,
+    `background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' id='basic-with-xmls'/>");`,
+    { from: "input.css", encode: false }
+  );
+});
 
-  it("should transform result svg into url", () => {
-    return compare(
-      `
-      @svg-load icon url(fixtures/basic.svg) {
-          fill: #fff;
+test("should transform result svg into url", () => {
+  return compare(
+    `
+    @svg-load icon url(fixtures/basic.svg) {
+        fill: #fff;
+    }
+    background: svg-load('fixtures/basic.svg', fill=#fff);
+    background: svg-inline(icon);
+    `,
+    `
+    background: url(basic.svg: transformed content);
+    background: url(basic.svg: transformed content);
+    `,
+    {
+      from: "input.css",
+      encode: false,
+      xmlns: false,
+      transform(result, file) {
+        expect(result).toEqual('<svg id="basic" fill="#fff"/>');
+        return file.split(/\\|\//).pop() + ": transformed content";
       }
-      background: svg-load('fixtures/basic.svg', fill=#fff);
-      background: svg-inline(icon);
-      `,
-      `
-      background: url(basic.svg: transformed content);
-      background: url(basic.svg: transformed content);
-      `,
-      {
-        from: 'input.css',
-        encode: false,
-        xmlns: false,
-        transform(result, file) {
-          expect(result).toEqual('<svg id="basic" fill="#fff"/>');
-          return file.split(/\\|\//).pop() + ": transformed content";
-        }
-      }
-    );
-  });
+    }
+  );
+});
 
-  it("should encode result svg", () => {
-    return compare(
-      `
-      @svg-load icon url(fixtures/basic.svg) {}
-      background: svg-load('fixtures/basic.svg');
-      background: svg-inline(icon);
-      `,
-      `
-      background: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/%3E");
-      background: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/%3E");
-      `,
-      { from: 'input.css' }
-    );
-  });
+test("should encode result svg", () => {
+  return compare(
+    `
+    @svg-load icon url(fixtures/basic.svg) {}
+    background: svg-load('fixtures/basic.svg');
+    background: svg-inline(icon);
+    `,
+    `
+    background: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/%3E");
+    background: url("data:image/svg+xml;charset=utf-8,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' id='basic'/%3E");
+    `,
+    { from: "input.css" }
+  );
+});
 
-  it("should encode result svg with custom encoder", () => {
-    return compare(
-      `
-      @svg-load icon url(fixtures/basic.svg) {}
-      background: svg-load('fixtures/basic.svg');
-      background: svg-inline(icon);
-      `,
-      `
-      background: url("data:image/svg+xml;charset=utf-8,1234567890");
-      background: url("data:image/svg+xml;charset=utf-8,1234567890");
-      `,
-      {
-        from: 'input.css',
-        encode(code) {
-          expect(code).toEqual(
-            `<svg xmlns="http://www.w3.org/2000/svg" id="basic"/>`
-          );
-          return "1234567890";
-        }
+test("should encode result svg with custom encoder", () => {
+  return compare(
+    `
+    @svg-load icon url(fixtures/basic.svg) {}
+    background: svg-load('fixtures/basic.svg');
+    background: svg-inline(icon);
+    `,
+    `
+    background: url("data:image/svg+xml;charset=utf-8,1234567890");
+    background: url("data:image/svg+xml;charset=utf-8,1234567890");
+    `,
+    {
+      from: "input.css",
+      encode(code) {
+        expect(code).toEqual(
+          `<svg xmlns="http://www.w3.org/2000/svg" id="basic"/>`
+        );
+        return "1234567890";
       }
-    );
-  });
+    }
+  );
+});
 
-  it('should combine results of "encode" and "transform"', () => {
-    return compare(
-      `background: svg-load('fixtures/basic.svg');`,
-      `background: url([transform: encode]);`,
-      {
-        from: 'input.css',
-        encode() {
-          return "encode";
-        },
-        transform(code) {
-          return `[transform: ${code}]`;
-        }
+test('should combine results of "encode" and "transform"', () => {
+  return compare(
+    `background: svg-load('fixtures/basic.svg');`,
+    `background: url([transform: encode]);`,
+    {
+      from: "input.css",
+      encode() {
+        return "encode";
+      },
+      transform(code) {
+        return `[transform: ${code}]`;
       }
-    );
-  });
+    }
+  );
+});
 
-  it("should remove fill attributes with removeFill: true", () => {
-    return compare(
-      `background: svg-load('fixtures/fill.svg', fill="#fff");`,
-      `background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' fill='#fff'> <path/> </svg>");`,
-      {
-        from: 'input.css',
-        removeFill: true,
-        encode: false
-      }
-    );
-  });
+test("should remove fill attributes with removeFill: true", () => {
+  return compare(
+    `background: svg-load('fixtures/fill.svg', fill="#fff");`,
+    `background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' fill='#fff'> <path/> </svg>");`,
+    {
+      from: "input.css",
+      removeFill: true,
+      encode: false
+    }
+  );
+});
 
-  it("should remove fill attributes with removeFill: RegExp", () => {
-    return compare(
-      `
-      background: svg-load('fixtures/fill.svg', fill="#fff");
-      background: svg-load('fixtures/fill-icon.svg', fill="#fff");
-      `,
-      `
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' fill='#fff'> <path fill='#000'/> </svg>");
-      background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' fill='#fff'> <rect/> </svg>");
-      `,
-      {
-        from: 'input.css',
-        removeFill: /-icon/,
-        encode: false
-      }
-    );
-  });
+test("should remove fill attributes with removeFill: RegExp", () => {
+  return compare(
+    `
+    background: svg-load('fixtures/fill.svg', fill="#fff");
+    background: svg-load('fixtures/fill-icon.svg', fill="#fff");
+    `,
+    `
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' fill='#fff'> <path fill='#000'/> </svg>");
+    background: url("data:image/svg+xml;charset=utf-8,<svg xmlns=\'http://www.w3.org/2000/svg\' fill='#fff'> <rect/> </svg>");
+    `,
+    {
+      from: "input.css",
+      removeFill: /-icon/,
+      encode: false
+    }
+  );
 });
